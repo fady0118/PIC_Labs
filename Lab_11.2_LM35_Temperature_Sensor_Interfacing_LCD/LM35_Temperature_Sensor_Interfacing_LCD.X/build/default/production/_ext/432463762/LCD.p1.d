254929807dcda834468e376f43c09bb23b257d9c
Fady Abd Elshahid <fady0118@gmail.com>
2024-05-20 16:00:30 +0300

    update Lab11.2 Code

---
+++ Lab_11.2_LM35_Temperature_Sensor_Interfacing_LCD/LM35_Temperature_Sensor_Interfacing_LCD.X/build/default/production/_ext/432463762/LCD.p1.d
@@ -0,0 +1,4 @@
+build/default/production/_ext/432463762/LCD.p1:  \
+../../Lab_09.1_UART_LCD_strings/UART_RX.X/LCD.c  \
+../../Lab_09.1_UART_LCD_strings/UART_RX.X/LCD.h  \
+../../Lab_09.1_UART_LCD_strings/UART_RX.X/Config.h 
--- Lab_11.2_LM35_Temperature_Sensor_Interfacing_LCD/LM35_Temperature_Sensor_Interfacing_LCD.X/main.c
+++ Lab_11.2_LM35_Temperature_Sensor_Interfacing_LCD/LM35_Temperature_Sensor_Interfacing_LCD.X/main.c
@@ -22,5 +22,54 @@ void ADC_Init(void);
 
 //------------Main_Routine--------------
 void main(void) {
+    ADC_Init();
+    LCD_Init();
+    LCD_Clear();
+    while(1){
+    // Read The ADC
+    ADC_Read(3);    // initiate conversion and update result variable
+    // Calculate The Temperature 
+        // The ADC is 10-bit meaning the resolution is 5v/2^10 =4.88mv
+        // LM35 datasheet states a Linear + 10-mV/°C Scale Factor
+        // Temperature is the A/D digital value * 0.488°C
+    Temperature = result*0.488;
+    // Convert The Temperature From Float To String
+    sprintf(Temp_String, "Temperature is -> %.3fc \r\n", Temperature);
+    // Print Out The Temperature
+    LCD_Set_Cursor(1,1);
+    LCD_Write_String(" Temperature Is ");
+    LCD_Set_Cursor(2,1);
+    LCD_Write_String(Temp_String);
+    __delay_ms(10);
+    }
     return;
+}
+//-----------Functions_Implementations------------
+//--------ADC_initialization-------------
+void ADC_Init(void){
+ADCON0|=0x59;   // Turn ADC ON, Select AN3 Channel, ADC Clock = Fosc/8
+ADCON1|=0x80;   // All 8 Channels Are Analog, Result is "Right-Justified"
+//interrupts
+ADIF=0;
+ADIE=1;
+PEIE=1;
+GIE=1;
+}
+//-----------AD_conversion-------------
+// Input: Analog channel number
+void ADC_Read(uint8_t AN){
+    if(AN>=0 && AN<=7){ // Check Channel Number Validity
+    ADCON0&=~0x38;      // clear channel selection
+    ADCON0|=AN<<3;      // channel selection
+    __delay_us(30);     // Wait The Acquisition Time
+    ADCON0|=0x04;       // Set GO/DONE    
+    }
+    else {result=0;}
+}
+//------------ISR----------------
+void __interrupt() ISR(void){
+    if(ADIF){
+    ADIF=0;             // clear interrupt flag
+    result=(ADRESH<<8)+ADRESL;
+    }
 }
