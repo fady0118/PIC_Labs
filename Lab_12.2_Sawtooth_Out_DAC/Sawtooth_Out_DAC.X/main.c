47025115826f5bb9babc75b5af2ff53d4e2fb601
Fady Abd Elshahid <fady0118@gmail.com>
2024-05-21 15:09:52 +0300

    initialize Lab12.2

---
+++ Lab_12.2_Sawtooth_Out_DAC/Sawtooth_Out_DAC.X/main.c
@@ -0,0 +1,17 @@
+/* Sawtooth_Out_DAC
+ * File:   main.c
+ * Author: fady
+ *
+ * Created on May 21, 2024, 2:07 PM
+ */
+
+//---------Preprocessor---------
+#include <xc.h>
+#include <stdint.h>
+#include "Config.h"
+#define _XTAL_FREQ 4000000
+#define DAC_OUT PORTB
+
+void main(void) {
+    return;
+}
