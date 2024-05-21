0dc493cd90605d9c623a8c399f33a77341da6994
Fady Abd Elshahid <fady0118@gmail.com>
2024-05-21 15:35:11 +0300

    update Lab_12.2

--- Lab_12.2_Sawtooth_Out_DAC/Sawtooth_Out_DAC_using_Timer.X/main.c
+++ Lab_12.2_Sawtooth_Out_DAC/Sawtooth_Out_DAC_using_Timer.X/main.c
@@ -1,9 +1,13 @@
-/* Sawtooth_Out_DAC
+/* Sawtooth_Out_DAC_using_Timer
  * File:   main.c
  * Author: fady
  *
- * Created on May 21, 2024, 2:07 PM
+ * Created on May 21, 2024, 2:31 PM
  */
+
+// [This is a modification to the Sawtooth_Out_DAC Lab
+//  but we'll be using Timer delays instead for better accuracy]
+
 
 // This code is dedicated for Generating Sawtooth Waveform With DAC  @ 10Hz
 // Fout=1/(Number of samples * Ts)
@@ -16,13 +20,18 @@
 #define _XTAL_FREQ 4000000
 #define DAC_OUT PORTB
 
+//-----------Timer1_Init-------------
+void Timer1_Init(void){
+
+
+}
+
 void main(void) {
     TRISB = 0x00;       // For 8-Bit DAC Out
     DAC_OUT = 0x00;     // initial state low
     uint8_t data=0;     // data variable 
     while(1){
-        DAC_OUT=data++;
-        __delay_us(390);
+
     }
     return;
 }
---
+++ Lab_12.2_Sawtooth_Out_DAC/Sawtooth_Out_DAC_using_Timer.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=Sawtooth_Out_DAC_using_Timer.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Sawtooth_Out_DAC_using_Timer.X.production.hex
